e4273fe1f4d402386ed61bbdbef3664b800788c3
Mmeli Ntshakala <mmeliroot@gmail.com>
2025-05-29 19:15:53 +0200

    full translation

--- Api.py
+++ Api.py
@@ -1,5 +1,16 @@
 import googletrans import Translator
 
+translator = Translator()
 
-# print languages
-print(googletranslate.LANGUAGES)
+def zulu_translate(massage):
+    translation = translator.translate(massage, src='en', dest='zu')
+    return translation.text
+
+"""
+    Now testing and calling the function to see the functionality
+    This is us taking depth 
+"""
+
+english_text = "take the table"
+zulu_translation = zulu_translate(english_text)
+print(f"English: {english_text} \n Zulu: {zulu_translation}")
